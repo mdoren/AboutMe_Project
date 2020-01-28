80084c36df6499a52a68a4044e34b7f49caf91b9
Matt Doren <mattdoren62@gmail.com>
2020-01-27 21:48:00 -0600

    Create AboutMe.kt

---
+++ AboutMe.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("My name is Matt. I started to learn programming in 2017.")
+    println("I hope to learn as much as I can about game development at MCC.")
+    println("I have a personal project that I am very excited to get the ball rolling on.")
+    println("I hope to one day make a sequel of an old Dreamcast game that is very nostalgic to me.")
+}
